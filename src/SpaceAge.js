405c6f95562d550b62a75b3bbcbc2d450f4b2098
Michael Larragueta <m.larragueta@yahoo.com>
2019-05-31 11:27:35 -0700

    Fix errors in printing age

--- src/SpaceAge.js
+++ src/SpaceAge.js
@@ -1,10 +1,43 @@
+import $ from "jquery";
+
 export class SpaceCalc{
-    constructor(year, month, day){
+    constructor(year, month, day) {
         this.year = year;
         this.month = month;
         this.day = day;
-        this.currentYear = 2019;
-        this.currentMonth = 5;
-        this.currentDay = 31;
     }
+    getDays(){
+        const currentDay = 31;
+        if((currentDay - this.day) >= 0) {
+            return 1;
+        }else return 0;
+    }
+    getMonth(){
+        const currentMonth = 5;
+        if((currentMonth - this.month) >= 0) {
+            return 1;
+        }else return 0;
+    }
+    getYear(){
+        const currentYear = 2019;
+        if((currentYear - this.year) >= 0){
+            return currentYear - this.year;
+        }else return 0;
+    }
+    getAge(bDay, bMonth, bYear){
+        var countDay = bDay;
+        var countMonth = bMonth;
+        var countYear = bYear;
+        if(countDay === 1 && countMonth === 1 && countYear >= 1){
+            return countYear;
+        }else if(countYear === 0){
+            return 0;
+        }else return countYear - 1;
+    }
+      printAge(age){
+        return ("You Are " + age + " years old on Earth");
+      }
+
 }
+
+
--- src/main.js
+++ src/main.js
@@ -1,21 +1,25 @@
-import SpaceCalc from './SpaceAge.js';
+import {SpaceCalc} from './SpaceAge.js';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-    $("#birthdate").submit(function(event){
-        //shows in year-month-day
-       let birthday = $("#bDate").val()
-        let dateHold = birthday.split("-");
-        let year = dateHold[0];
-        let month = dateHold[1];
-        let day = dateHold[2];
-        let ageCount = new SpaceCalc(year, month, day);
-        console.log(year);
-        console.log(month);
-        console.log(day);
-       event.preventDefault();
-    });
+  $("#birthdate").submit(function(event){
+    //shows in year-month-day
+    let birthday = $("#bDate").val();
+    let dateHold = birthday.split("-");
+    let year = parseInt(dateHold[0]);
+    let month = parseInt(dateHold[1]);
+    let day = parseInt(dateHold[2]);
+    let ageCount = new SpaceCalc(year, month, day);
+    let dayCount = ageCount.getDays();
+    let monthCount = ageCount.getMonth();
+    let yearCount = ageCount.getYear();
+    let trueAge = ageCount.getAge(dayCount, monthCount, yearCount);
+    $("#result").text(ageCount.printAge(trueAge));
+
+
+    event.preventDefault();
+  });
 });
